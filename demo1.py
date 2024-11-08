5b975c1c8bf1bcc1d48921fd91bf2fc669206261
Sushree <sushreepattanayak@gmail.com>
2024-11-08 11:32:31 -0500

    created my second python programming

---
+++ demo1.py
@@ -0,0 +1,16 @@
+# Basic Arithmatic calculation
+a = 10
+b = 5
+
+# Addition
+print("Addition:", a + b)
+
+# Subtraction
+print("Subtraction:", a - b)
+
+# Multiplication
+print("Multiplication:", a * b)
+
+# Division
+print("Division:", a / b)
+
